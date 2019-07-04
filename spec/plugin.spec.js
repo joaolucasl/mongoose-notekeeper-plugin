e32504dd1b222fcddd4ac8bc6055f8c24a4e96a6
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2019-07-04 12:38:43 -0300

    Fix async operations in spec assertions

--- spec/plugin.spec.js
+++ spec/plugin.spec.js
@@ -1,59 +1,72 @@
-const { Schema, Mongoose } = require('mongoose')
-const mongoose = new Mongoose()
-const { MongoMemoryServer } = require('mongodb-memory-server')
+const { Schema, Mongoose } = require("mongoose");
+const mongoose = new Mongoose();
+const { MongoMemoryServer } = require("mongodb-memory-server");
 
-const notekeeperPlugin = require('../dist/index')
+const notekeeperPlugin = require("../dist/index");
 
-describe('Mongoose Notekeeper Plugin', () => {
-  let mongod
+describe("Mongoose Notekeeper Plugin", () => {
+  describe("When defining a new Schema and using the plugin to track a field named `name`", () => {
+    let mongod;
 
-  beforeAll(async () => {
-    mongod = new MongoMemoryServer()
-    const uri = await mongod.getConnectionString()
-    await mongoose.connect(uri)
-  })
+    beforeAll(async () => {
+      mongod = new MongoMemoryServer();
+      const uri = await mongod.getConnectionString();
+      await mongoose.connect(uri);
+    });
 
-  afterAll(async (done) => {
-    await mongoose.disconnect()
-    await mongod.stop()
-  })
+    afterAll(async () => {
+      await mongoose.disconnect();
+      await mongod.stop();
+    });
 
-  describe('When defining a new Schema and using the plugin to track a field named `name`', () => {
     const PokemonSchema = new Schema({
       name: String,
-      type: String,
-    })
+      type: String
+    });
 
-    notekeeperPlugin(PokemonSchema, { fields: ['name'] })
+    notekeeperPlugin(PokemonSchema, { fields: ["name"] });
+    const PokemonModel = mongoose.connection.model("pokemon", PokemonSchema);
 
-    const PokemonModel = mongoose.connection.model('pokemon', PokemonSchema)
-
-    it('created documents should have a `history` field related to the required fields', async function () {
+    it("created documents should have a `history` field related to the required fields", async function() {
       const pokemon = await PokemonModel.create({
-        name: 'Charmander',
-        type: 'Fire'
-      })
+        name: "Charmander",
+        type: "Fire"
+      });
 
-      expect(pokemon).toBeDefined()
-      expect(pokemon.name_history).toBeDefined()
-      return expect(pokemon.name_history).toEqual(expect.any(Array))
-    })
+      expect(pokemon).toBeDefined();
+      expect(pokemon.name_history).toBeDefined();
+      return expect(pokemon.name_history).toEqual(expect.any(Array));
+    });
 
-    it('after updating a field being watched, a new history entry must be added', async function () {
+    it("after updating a field being watched, a new history entry must be added", async function() {
       const charmander = await PokemonModel.create({
-        name: 'Charmander',
-        type: 'Fire'
-      })
+        name: "Charmander",
+        type: "Fire"
+      });
 
-      expect(charmander.name_history.length).toBe(0)
+      expect(charmander.name_history.length).toBe(0);
 
-      charmander.name = 'Charmeleon'
-      charmander.save()
+      charmander.name = "Charmeleon";
+      await charmander.save();
 
-      const charmeleon = PokemonModel.findOne({ name: 'Charmeleon' })
-      expect(charmeleon.name_history.length).toBe(1)
+      const charmeleon = await PokemonModel.findOne({ name: "Charmeleon" });
 
-      return expect(pokemon.name_history).toEqual(expect.any(Array))
-    })
-  })
-})
+      expect(charmeleon.name_history.length).toBe(1);
+
+      return expect(pokemon.name_history).toEqual(expect.any(Array));
+    });
+  });
+
+  describe("When defining a new Schema and using the plugin with invalid field definitions ", () => {
+    const PokemonSchema = new Schema({
+      name: String,
+      type: String
+    });
+
+    it("should throw an error while validating the schema", () => {
+      return expect(() =>
+        notekeeperPlugin(PokemonSchema, { fields: [] })
+      ).toThrow();
+    });
+  });
+});
